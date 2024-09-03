7318e12331205896980bbca6a5e9db5fbd3d5c25
Cha Hoseong <hoseong@chahoseong.com>
2024-09-03 21:41:22 +0900

    11.02 search a sorted array for entry equal to its index

--- epi_judge_cpp/search_entry_equal_to_index.cc
+++ epi_judge_cpp/search_entry_equal_to_index.cc
@@ -6,31 +6,48 @@
 using std::vector;
 
 int SearchEntryEqualToItsIndex(const vector<int>& A) {
-  // TODO - you fill in here.
-    
-  return 0;
-}
-void SearchEntryEqualToItsIndexWrapper(TimedExecutor& executor,
-                                       const vector<int>& A) {
-  int result = executor.Run([&] { return SearchEntryEqualToItsIndex(A); });
+	int left = 0;
+	int right = static_cast<int>(std::size(A));
 
-  if (result != -1) {
-    if (A[result] != result) {
-      throw TestFailure("Entry does not equal to its index");
-    }
-  } else {
-    for (int i = 0; i < A.size(); ++i) {
-      if (A[i] == i) {
-        throw TestFailure("There are entries which equal to its index");
-      }
-    }
-  }
+	while (left < right) {
+		int mid = (left + right) / 2;
+
+		if (A[mid] == mid) {
+			return mid;
+		}
+		else if (A[mid] < mid) {
+			left = mid + 1;
+		}
+		else {
+			right = mid;
+		}
+	}
+
+	return -1;
+}
+
+void SearchEntryEqualToItsIndexWrapper(TimedExecutor& executor,
+	const vector<int>& A) {
+	int result = executor.Run([&] { return SearchEntryEqualToItsIndex(A); });
+
+	if (result != -1) {
+		if (A[result] != result) {
+			throw TestFailure("Entry does not equal to its index");
+		}
+	}
+	else {
+		for (int i = 0; i < A.size(); ++i) {
+			if (A[i] == i) {
+				throw TestFailure("There are entries which equal to its index");
+			}
+		}
+	}
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"executor", "A"};
-  return GenericTestMain(
-      args, "search_entry_equal_to_index.cc", "search_entry_equal_to_index.tsv",
-      &SearchEntryEqualToItsIndexWrapper, DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "executor", "A" };
+	return GenericTestMain(
+		args, "search_entry_equal_to_index.cc", "search_entry_equal_to_index.tsv",
+		&SearchEntryEqualToItsIndexWrapper, DefaultComparator{}, param_names);
 }
