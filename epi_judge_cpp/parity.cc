1b63f7c6e2856a5e12298c7182c7181534ddd9e7
Cha Hoseong <hoseong@chahoseong.com>
2025-08-08 10:24:02 +0900

    4.0, 4.1

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,12 +1,19 @@
 #include "test_framework/generic_test.h"
+#include <array>
+
+// lookup table을 사용하면 더 빠르게 패리티를 구할 수 있다.
 short Parity(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+	short result = 0;
+	while (x) {
+		result ^= 1;
+		x &= (x - 1);
+	}
+	return result;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"x"};
-  return GenericTestMain(args, "parity.cc", "parity.tsv", &Parity,
-                         DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "x" };
+	return GenericTestMain(args, "parity.cc", "parity.tsv", &Parity,
+		DefaultComparator{}, param_names);
 }
