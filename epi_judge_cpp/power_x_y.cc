58419b0dc44e3c2fb343ee607073787124f6a741
Cha Hoseong <hoseong@chahoseong.com>
2025-08-11 10:07:07 +0900

    4.7 pow(x, y)

--- epi_judge_cpp/power_x_y.cc
+++ epi_judge_cpp/power_x_y.cc
@@ -1,12 +1,63 @@
 #include "test_framework/generic_test.h"
-double Power(double x, int y) {
-  // TODO - you fill in here.
-  return 0.0;
+
+//double Power(double x, int y) {
+//	if (y == 0) {
+//		return 1.0;
+//	}
+//	
+//	double result = 1.0;
+//	for (int i = 1; i <= std::abs(y); ++i) {
+//		result *= x;
+//	}
+//	return y < 0 ? 1.0 / result : result;
+//}
+
+//double Power(double x, int y) {
+//	int y_abs = std::abs(y);
+//
+//	if (y_abs == 2) {
+//		double result = x * x;
+//		return y < 0 ? (1.0 / result) : result;
+//	}
+//	else if (y_abs == 1) {
+//		return y < 0 ? (1.0 / x) : x;
+//	}
+//	else if (y == 0) {
+//		return 1;
+//	}
+//
+//	double half = Power(x, y / 2);
+//	if (y % 2 == 0) {
+//		return half * half;
+//	}
+//	else {
+//		double r = half * half;
+//		return y < 0 ? (r * (1.0 / x)) : r * x;
+//	}
+//}
+
+// solution
+double Power(double x, int y)
+{
+	double result = 1.0;
+	long long power = y;
+	if (y < 0) {
+		power = -power;
+		x = 1.0 / x;
+	}
+	while (power) {
+		if (power & 1) {
+			result *= x;
+		}
+		x *= x;
+		power >>= 1;
+	}
+	return result;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"x", "y"};
-  return GenericTestMain(args, "power_x_y.cc", "power_x_y.tsv", &Power,
-                         DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "x", "y" };
+	return GenericTestMain(args, "power_x_y.cc", "power_x_y.tsv", &Power,
+		DefaultComparator{}, param_names);
 }
--- problem_mapping.js
+++ problem_mapping.js
@@ -2,7 +2,7 @@ problem_mapping = {
     "Chapter 04: Primitive Types": {
         "4.00 Bootcamp: Primitive Types": {
             "C++: count_bits.cc": {
-                "passed": 0,
+                "passed": 10001,
                 "total": 10001
             },
             "Java: CountBits.java": {
@@ -16,7 +16,7 @@ problem_mapping = {
         },
         "4.01 Computing the parity of a word": {
             "C++: parity.cc": {
-                "passed": 0,
+                "passed": 10000,
                 "total": 10000
             },
             "Java: Parity.java": {
@@ -100,7 +100,7 @@ problem_mapping = {
         },
         "4.07 Compute pow(x,y)": {
             "C++: power_x_y.cc": {
-                "passed": 0,
+                "passed": 10002,
                 "total": 10002
             },
             "Java: PowerXY.java": {
