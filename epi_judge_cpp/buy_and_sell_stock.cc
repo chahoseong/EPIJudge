7e1f8ef8f34b06cf437cb74c15301b700a79ffdf
Cha Hoseong <hoseong@chahoseong.com>
2025-08-25 10:09:31 +0900

    5.06

--- epi_judge_cpp/buy_and_sell_stock.cc
+++ epi_judge_cpp/buy_and_sell_stock.cc
@@ -1,16 +1,29 @@
 #include <vector>
-
 #include "test_framework/generic_test.h"
+
 using std::vector;
+
 double BuyAndSellStockOnce(const vector<double>& prices) {
-  // TODO - you fill in here.
-  return 0.0;
+	size_t lowestIndex = 0;
+	double bestProfit = 0.0;
+
+	for (size_t day = 1; day < prices.size(); ++day) {
+		double profit = prices[day] - prices[lowestIndex];
+		if (profit > bestProfit) {
+			bestProfit = profit;
+		}
+		if (prices[day] < prices[lowestIndex]) {
+			lowestIndex = day;
+		}
+	}
+
+	return bestProfit;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"prices"};
-  return GenericTestMain(args, "buy_and_sell_stock.cc",
-                         "buy_and_sell_stock.tsv", &BuyAndSellStockOnce,
-                         DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "prices" };
+	return GenericTestMain(args, "buy_and_sell_stock.cc",
+		"buy_and_sell_stock.tsv", &BuyAndSellStockOnce,
+		DefaultComparator{}, param_names);
 }
