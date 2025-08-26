46ee5a7eecea5b8deca576201b2534216ebdefae
Cha Hoseong <hoseong@chahoseong.com>
2025-08-26 10:17:11 +0900

    5.12

--- epi_judge_cpp/offline_sampling.cc
+++ epi_judge_cpp/offline_sampling.cc
@@ -2,54 +2,66 @@
 #include <functional>
 #include <iterator>
 #include <vector>
+#include <random>
 
 #include "test_framework/generic_test.h"
 #include "test_framework/random_sequence_checker.h"
 #include "test_framework/timed_executor.h"
+
 using std::bind;
 using std::vector;
+
+// 배열에서 크기가 k인 배열을 생성하는 문제
+// (크기가 k인 부분 배열을 생성하는 확률을 동일해야 함)
 void RandomSampling(int k, vector<int>* A_ptr) {
-  // TODO - you fill in here.
-  return;
+	vector<int>& v = *A_ptr;
+	std::random_device rd;
+	std::mt19937 generator(rd());
+
+	for (int i = 0; i < k; ++i) {
+		std::uniform_int_distribution<int> distributor(i, v.size() - 1);
+		std::swap(v[i], v[distributor(generator)]);
+	}
 }
+
 bool RandomSamplingRunner(TimedExecutor& executor, int k, vector<int> A) {
-  using namespace test_framework;
-  vector<vector<int>> results;
+	using namespace test_framework;
+	vector<vector<int>> results;
 
-  executor.Run([&] {
-    for (int i = 0; i < 100000; ++i) {
-      RandomSampling(k, &A);
-      results.emplace_back(begin(A), begin(A) + k);
-    }
-  });
+	executor.Run([&] {
+		for (int i = 0; i < 100000; ++i) {
+			RandomSampling(k, &A);
+			results.emplace_back(begin(A), begin(A) + k);
+		}
+		});
 
-  int total_possible_outcomes = BinomialCoefficient(A.size(), k);
-  sort(begin(A), end(A));
-  vector<vector<int>> combinations;
-  for (int i = 0; i < BinomialCoefficient(A.size(), k); ++i) {
-    combinations.emplace_back(ComputeCombinationIdx(A, A.size(), k, i));
-  }
-  vector<int> sequence;
-  for (vector<int> result : results) {
-    sort(begin(result), end(result));
-    sequence.emplace_back(
-        distance(begin(combinations),
-                 find(begin(combinations), end(combinations), result)));
-  }
-  return CheckSequenceIsUniformlyRandom(sequence, total_possible_outcomes,
-                                        0.01);
+	int total_possible_outcomes = BinomialCoefficient(A.size(), k);
+	sort(begin(A), end(A));
+	vector<vector<int>> combinations;
+	for (int i = 0; i < BinomialCoefficient(A.size(), k); ++i) {
+		combinations.emplace_back(ComputeCombinationIdx(A, A.size(), k, i));
+	}
+	vector<int> sequence;
+	for (vector<int> result : results) {
+		sort(begin(result), end(result));
+		sequence.emplace_back(
+			distance(begin(combinations),
+				find(begin(combinations), end(combinations), result)));
+	}
+	return CheckSequenceIsUniformlyRandom(sequence, total_possible_outcomes,
+		0.01);
 }
 
 void RandomSamplingWrapper(TimedExecutor& executor, int k,
-                           const vector<int>& A) {
-  RunFuncWithRetries(
-      bind(RandomSamplingRunner, std::ref(executor), k, std::cref(A)));
+	const vector<int>& A) {
+	RunFuncWithRetries(
+		bind(RandomSamplingRunner, std::ref(executor), k, std::cref(A)));
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"executor", "k", "A"};
-  return GenericTestMain(args, "offline_sampling.cc", "offline_sampling.tsv",
-                         &RandomSamplingWrapper, DefaultComparator{},
-                         param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "executor", "k", "A" };
+	return GenericTestMain(args, "offline_sampling.cc", "offline_sampling.tsv",
+		&RandomSamplingWrapper, DefaultComparator{},
+		param_names);
 }
