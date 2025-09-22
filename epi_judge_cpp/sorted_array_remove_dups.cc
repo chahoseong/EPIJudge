937ce578ea81aa059eb0d16a9e7b5fa0b13400ee
Cha Hoseong <hoseong@chahoseong.com>
2025-09-22 16:39:43 +0900

    5.05

--- epi_judge_cpp/sorted_array_remove_dups.cc
+++ epi_judge_cpp/sorted_array_remove_dups.cc
@@ -2,22 +2,36 @@
 
 #include "test_framework/generic_test.h"
 #include "test_framework/timed_executor.h"
+
 using std::vector;
+
 // Returns the number of valid entries after deletion.
 int DeleteDuplicates(vector<int>* A_ptr) {
-  // TODO - you fill in here.
-  return 0;
+	vector<int>& arr = *A_ptr;
+	int duplicatedCount = 0;
+
+	for (size_t i = 1; i < arr.size(); ++i) {
+		if (arr[i - 1] == arr[i]) {
+			++duplicatedCount;
+		}
+		else {
+			arr[i - duplicatedCount] = arr[i];
+		}
+	}
+
+	return static_cast<int>(arr.size() - duplicatedCount);
 }
+
 vector<int> DeleteDuplicatesWrapper(TimedExecutor& executor, vector<int> A) {
-  int end = executor.Run([&] { return DeleteDuplicates(&A); });
-  A.resize(end);
-  return A;
+	int end = executor.Run([&] { return DeleteDuplicates(&A); });
+	A.resize(end);
+	return A;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"executor", "A"};
-  return GenericTestMain(
-      args, "sorted_array_remove_dups.cc", "sorted_array_remove_dups.tsv",
-      &DeleteDuplicatesWrapper, DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "executor", "A" };
+	return GenericTestMain(
+		args, "sorted_array_remove_dups.cc", "sorted_array_remove_dups.tsv",
+		&DeleteDuplicatesWrapper, DefaultComparator{}, param_names);
 }
