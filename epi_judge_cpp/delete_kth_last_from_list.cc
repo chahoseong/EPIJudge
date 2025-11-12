2f6c879f0389627fb018f9ca7ba785e2ad830f4b
Cha Hoseong <hoseong@chahoseong.com>
2025-11-12 20:03:48 +0900

    7.07

--- epi_judge_cpp/delete_kth_last_from_list.cc
+++ epi_judge_cpp/delete_kth_last_from_list.cc
@@ -5,16 +5,29 @@
 using std::shared_ptr;
 
 // Assumes L has at least k nodes, deletes the k-th last node in L.
-shared_ptr<ListNode<int>> RemoveKthLast(const shared_ptr<ListNode<int>>& L,
-                                        int k) {
-  // TODO - you fill in here.
-  return nullptr;
+shared_ptr<ListNode<int>> RemoveKthLast(const shared_ptr<ListNode<int>>& L, int k) {
+	shared_ptr<ListNode<int>> head = make_shared<ListNode<int>>(0, L);
+	
+	auto front = head->next;
+	for (int i = 0; i < k; ++i) {
+		front = front->next;
+	}
+
+	auto rear = head;
+	while (front != nullptr) {
+		front = front->next;
+		rear = rear->next;
+	}
+
+	rear->next = rear->next->next;
+
+	return head->next;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"L", "k"};
-  return GenericTestMain(args, "delete_kth_last_from_list.cc",
-                         "delete_kth_last_from_list.tsv", &RemoveKthLast,
-                         DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "L", "k" };
+	return GenericTestMain(args, "delete_kth_last_from_list.cc",
+		"delete_kth_last_from_list.tsv", &RemoveKthLast,
+		DefaultComparator{}, param_names);
 }
