b0b12cdf218944cf2ed153d590238084420e7a60
Cha Hoseong <hoseong@chahoseong.com>
2025-11-17 20:39:47 +0900

    7.10, 7.11

--- epi_judge_cpp/even_odd_list_merge.cc
+++ epi_judge_cpp/even_odd_list_merge.cc
@@ -1,14 +1,35 @@
 #include "list_node.h"
 #include "test_framework/generic_test.h"
+
 shared_ptr<ListNode<int>> EvenOddMerge(const shared_ptr<ListNode<int>>& L) {
-  // TODO - you fill in here.
-  return nullptr;
+	if (!L) {
+		return nullptr;
+	}
+
+	shared_ptr<ListNode<int>> even_head = make_shared<ListNode<int>>(0, nullptr);
+	shared_ptr<ListNode<int>> odd_head = make_shared<ListNode<int>>(0, nullptr);
+	shared_ptr<ListNode<int>> tails[2] = { even_head, odd_head };
+
+	auto cursor = L;
+	int i = 0;
+
+	while (cursor) {
+		tails[i]->next = cursor;
+		tails[i] = cursor;
+		cursor = cursor->next;
+		tails[i]->next = nullptr;
+		i = (i + 1) % 2;
+	}
+
+	tails[0]->next = odd_head->next;
+
+	return even_head->next;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"L"};
-  return GenericTestMain(args, "even_odd_list_merge.cc",
-                         "even_odd_list_merge.tsv", &EvenOddMerge,
-                         DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "L" };
+	return GenericTestMain(args, "even_odd_list_merge.cc",
+		"even_odd_list_merge.tsv", &EvenOddMerge,
+		DefaultComparator{}, param_names);
 }
--- epi_judge_cpp/is_list_palindromic.cc
+++ epi_judge_cpp/is_list_palindromic.cc
@@ -2,14 +2,40 @@
 #include "test_framework/generic_test.h"
 
 bool IsLinkedListAPalindrome(shared_ptr<ListNode<int>> L) {
-  // TODO - you fill in here.
-  return true;
+	auto fast = L;
+	auto slow = L;
+
+	while (fast && fast->next) {
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+
+	shared_ptr<ListNode<int>> R;
+	while (slow) {
+		auto T = slow;
+		slow = slow->next;
+		T->next = R;
+		R = T;
+	}
+
+	auto x = L;
+	auto y = R;
+
+	while (x && y) {
+		if (x->data != y->data) {
+			return false;
+		}
+		x = x->next;
+		y = y->next;
+	}
+
+	return true;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"L"};
-  return GenericTestMain(args, "is_list_palindromic.cc",
-                         "is_list_palindromic.tsv", &IsLinkedListAPalindrome,
-                         DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "L" };
+	return GenericTestMain(args, "is_list_palindromic.cc",
+		"is_list_palindromic.tsv", &IsLinkedListAPalindrome,
+		DefaultComparator{}, param_names);
 }
